56107e0ff04900f9aa47107d67973a76dc663df7
Mohammad Rahimi <moyi.pary@gmail.com>
2023-03-04 01:53:00 +0330

    class based decorator was completed

---
+++ class_base_decorator.py
@@ -0,0 +1,26 @@
+class Decorator:
+    def __init__(self, *args, **kwargs):
+        print("starting the decorator")
+        self._args = args
+        self._kwargs = kwargs
+
+    def __call__(self, function):
+        self.function = function
+
+        def decorator(*args, **kwargs):
+            print(self.function(*args, **kwargs))
+            print(args)
+            print(kwargs)
+            return self.function(*args, **kwargs)
+
+        return decorator
+
+
+@Decorator(preson=45)
+def ali(name: str):
+    if name:
+        return name + " ali ali ali"
+    return 'ali ali ali'
+
+
+print(ali(name="no"))
